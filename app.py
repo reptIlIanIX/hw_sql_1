a012946c7aad6d738976e4c4fd8426637598cfc0
Den4ik <spawnslash@mail.ru>
2022-11-18 16:48:08 +0300

    placement

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify, json
+from flask import Flask, jsonify
 
-from functions import get_movie_by_title
+from functions import get_movie_by_title, get_movie_by_year, rating_by_keyword, get_movie_by_genre
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -10,6 +10,24 @@ app.config['JSON_AS_ASCII'] = False
 def page_movies(title):
     movie = get_movie_by_title(title)
     return jsonify(movie)
+
+
+@app.route("/movies/<int:from_year>/to/<int:to_year>")
+def range_year_movies(from_year, to_year):
+    year_range = get_movie_by_year(from_year, to_year)
+    return jsonify(year_range)
+
+
+@app.route("/rating/<rating>")
+def children_rating(rating):
+    all_ratings = rating_by_keyword(rating)
+    return jsonify(all_ratings)
+
+
+@app.route("/genre/<genre>")
+def movie_by_genre(genre):
+    genres = get_movie_by_genre(genre)
+    return jsonify(genres)
 
 
 if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 
+# датабазные функции
 def get_data_from_base():
     con = sqlite3.connect('netflix.db')  # Подключаемся к БД
     cur = con.cursor()  # Запускаем курсор, с помощью которого мы будем получать данные из БД
@@ -9,18 +10,6 @@ def get_data_from_base():
     data = cur.fetchall()  # С помощью этой функции получаем результат запроса в виде списка кортежей
     con.close()  # После выполнения запросов обязательно закрываем соединение с БД
     return data
-
-
-def get_movie_by_title(title):
-    for item in get_data_from_base():
-        d = {
-            "title": item[0],
-            "country": item[1],
-            "release_year": item[2],
-            "description": item[3]
-        }
-        if title in d["title"]:
-            return d
 
 
 def get_title_year_from_base():
@@ -33,7 +22,103 @@ def get_title_year_from_base():
     return data
 
 
-print(get_title_year_from_base())
+def get_data_for_age_rating():
+    con = sqlite3.connect('netflix.db')  # Подключаемся к БД
+    cur = con.cursor()  # Запускаем курсор, с помощью которого мы будем получать данные из БД
+    sqlite_query = f"Select title, rating, description FROM netflix"
+    result = cur.execute(sqlite_query)  # Выполняем запрос с помощью курсора
+    data = cur.fetchall()  # С помощью этой функции получаем результат запроса в виде списка кортежей
+    con.close()  # После выполнения запросов обязательно закрываем соединение с БД
+    return data
+
+
+def get_genre_from_base():
+    con = sqlite3.connect('netflix.db')  # Подключаемся к БД
+    cur = con.cursor()  # Запускаем курсор, с помощью которого мы будем получать данные из БД
+    sqlite_query = f"Select title, listed_in, description, release_year FROM netflix ORDER BY release_year DESC"
+    result = cur.execute(sqlite_query)  # Выполняем запрос с помощью курсора
+    data = cur.fetchall()  # С помощью этой функции получаем результат запроса в виде списка кортежей
+    con.close()  # После выполнения запросов обязательно закрываем соединение с БД
+    return data
+
+
+def get_actors_from_base():
+    con = sqlite3.connect('netflix.db')  # Подключаемся к БД
+    cur = con.cursor()  # Запускаем курсор, с помощью которого мы будем получать данные из БД
+    sqlite_query = f"Select title, netflix.cast FROM netflix"
+    result = cur.execute(sqlite_query)  # Выполняем запрос с помощью курсора
+    data = cur.fetchall()  # С помощью этой функции получаем результат запроса в виде списка кортежей
+    con.close()  # После выполнения запросов обязательно закрываем соединение с БД
+    return data
+
+
+def get_all_from_base():
+    con = sqlite3.connect('netflix.db')  # Подключаемся к БД
+    cur = con.cursor()  # Запускаем курсор, с помощью которого мы будем получать данные из БД
+    sqlite_query = f"Select title, type, release_year, listed_in, description FROM netflix"
+    result = cur.execute(sqlite_query)  # Выполняем запрос с помощью курсора
+    data = cur.fetchall()  # С помощью этой функции получаем результат запроса в виде списка кортежей
+    con.close()  # После выполнения запросов обязательно закрываем соединение с БД
+    return data
+
+
+# эппроутовские функции
+def get_movie_by_genre(genre):
+    genres = []
+    for item in get_genre_from_base():
+        if genre.lower() in item[1].lower():
+            g = {
+                "title": item[0],
+                "description": item[2],
+            }
+            if len(genres) < 10:
+                genres.append(g)
+    return genres
+
+
+def get_movie_by_rating():
+    movies = []
+    for item in get_data_for_age_rating():
+        r = {
+            "title": item[0],
+            "rating": item[1],
+
+        }
+        movies.append(r)
+    return movies
+
+
+def new_rating():
+    movies = []
+    for item in get_movie_by_rating():
+        if item['rating'] in ('G', 'TV-G'):
+            item['rating'] = 'children, family'
+        elif item['rating'] in ('PG', 'TV-PG', 'TV-14', 'PG-13'):
+            item['rating'] = 'family'
+        elif item['rating'] in ('R', 'TV-MA', 'NC-17'):
+            item['rating'] = 'adult'
+        movies.append(item)
+    return movies
+
+
+def rating_by_keyword(rating):
+    keywords = []
+    for item in new_rating():
+        if rating in item['rating']:
+            keywords.append(item)
+    return keywords
+
+
+def get_movie_by_title(title):
+    for item in get_data_from_base():
+        d = {
+            "title": item[0],
+            "country": item[1],
+            "release_year": item[2],
+            "description": item[3]
+        }
+        if title in d["title"]:
+            return d
 
 
 def get_movie_title_year():
@@ -55,4 +140,26 @@ def get_movie_by_year(from_year: int, to_year: int):
     return years
 
 
-print(get_movie_by_year(2010, 2016))
+# ШАГ 5
+def actors_cast(one, two):
+    actors = []
+    for item in get_actors_from_base():
+        if one in item[1] and two in item[1]:
+            actors.append(item)
+    if len(actors) > 2:
+        return actors
+    else:
+        pass
+
+
+# ШАГ 6
+def get_type(type, year: int, genre):
+    movies = []
+    for item in get_all_from_base():
+        if type.lower() in item[1].lower() and year == item[2] and genre.lower() in item[3].lower():
+            types = {
+                "title": item[0],
+                "description": item[4],
+            }
+            movies.append(types)
+    return movies
